58f91710501b0beeacd0c954217a9103ba10ef97
mmkn <mmkn@bkdmm.com>
2024-12-08 09:34:09 +0800

    add lark push message scripts.

---
+++ lark/push_message.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import requests, sys
+
+LARK_BOT_URL="LARK_WEB_HOOK"
+
+def send_lark(text, parse_mode):
+    headers = {'Content-Type': 'application/json;charset=utf-8'}
+    content = {"msg_type": parse_mode, "content": {"text": text}}
+    req = requests.post(LARK_BOT_URL, headers=headers, json=content)
+    if req.json()['code'] == 0:
+        print("lark send ok.")
+    else:
+        print(f"lark send error, content: {content}, req: {req.json()}")
+
+def main():
+    if len(sys.argv) != 2:
+        print("please input message.")
+        sys.exit(110)
+    print(len(sys.argv))
+    text = sys.argv[1]
+    send_lark(text, 'text')
+
+if __name__ == "__main__":
+    main()
